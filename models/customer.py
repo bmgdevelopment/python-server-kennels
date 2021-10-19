a421abfc9fee5c5b29e196df1e66b9be092dd46e
Brittany Garrett <brittanymgarrett@gmail.com>
2021-10-19 14:31:11 -0700

    Model dir initiated with data

---
+++ models/customer.py
@@ -0,0 +1,8 @@
+class Customer():
+
+    def __init__(self, id, name, address, animalId, email):
+        self.id = id
+        self.name = name
+        self.address = address
+        self.animalId = animalId
+        self.email = email
