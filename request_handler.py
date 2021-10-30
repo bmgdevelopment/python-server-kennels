256549e05a9589e3ba08564c34d40beaf2d8261f
Brittany Garrett <brittanymgarrett@gmail.com>
2021-10-30 13:30:40 -0500

    Updated

--- request_handler.py
+++ request_handler.py
@@ -10,28 +10,37 @@ import json
 # work together for a common purpose. In this case, that
 # common purpose is to respond to HTTP requests from a client.
 
-class HandleRequests(BaseHTTPRequestHandler):
+class HandleRequests(BaseHTTPRequestHandler): 
 
     def parse_url(self, path):
-        # Just like splitting a string in JavaScript. If the
-        # path is "/animals/1", the resulting list will
-        # have "" at index 0, "animals" at index 1, and "1"
-        # at index 2.
-        path_params = path.split("/") 
-        resource = path_params[1] 
-        id = None 
+        path_params = path.split("/")
+        resource = path_params[1]
 
-        # Try to get the item at index 2
-        try: 
-            # Convert the string "1" to the integer 1
-            # This is the new parseInt()
-            id = int(path_params[2]) 
-        except IndexError: 
-            pass  # No route parameter exists: /animals
-        except ValueError: 
-            pass  # Request had trailing slash: /animals/
+        # Check if there is a query string parameter
+        if "?" in resource:
+            # GIVEN: /customers?email=jenna@solis.com
 
-        return (resource, id)  # This is a tuple 
+            param = resource.split("?")[1]  # email=jenna@solis.com
+            resource = resource.split("?")[0]  # 'customers'
+            pair = param.split("=")  # [ 'email', 'jenna@solis.com' ]
+            key = pair[0]  # 'email'
+            value = pair[1]  # 'jenna@solis.com'
+
+            return ( resource, key, value )
+
+        # No query string parameter
+        else:
+            id = None
+
+            try:
+                id = int(path_params[2])
+            except IndexError:
+                pass  # No route parameter exists: /animals
+            except ValueError:
+                pass  # Request had trailing slash: /animals/
+
+            return (resource, id) #this is a tuple
+
 
     # Here's a class function
     def _set_headers(self, status): 
@@ -48,48 +57,88 @@ class HandleRequests(BaseHTTPRequestHandler):
         self.send_header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept') 
         self.end_headers() 
 
+
+    def do_GET(self):
+        self._set_headers(200)
+
+        response = {}
+
+        # Parse URL and store entire tuple in a variable
+        parsed = self.parse_url(self.path)
+
+        # Response from parse_url() is a tuple with 2
+        # items in it, which means the request was for
+        # `/animals` or `/animals/2`
+        if len(parsed) == 2:
+            ( resource, id ) = parsed
+
+            if resource == "animals":
+                if id is not None:
+                    response = f"{get_single_animal(id)}"
+                else:
+                    response = f"{get_all_animals()}"
+            elif resource == "customers":
+                if id is not None:
+                    response = f"{get_single_customer(id)}"
+                else:
+                    response = f"{get_all_customers()}"
+
+        # Response from parse_url() is a tuple with 3
+        # items in it, which means the request was for
+        # `/resource?parameter=value`
+        elif len(parsed) == 3:
+            ( resource, key, value ) = parsed
+
+            # Is the resource `customers` and was there a
+            # query parameter that specified the customer
+            # email as a filtering value?
+            if key == "email" and resource == "customers":
+                response = get_customers_by_email(value)
+
+        self.wfile.write(response.encode())
+
     # Here's a method on the class that overrides the parent's method.
     # It handles any GET request.
-    def do_GET(self): 
-        self._set_headers(200) 
-        response = {}  # Default response
+    # def do_GET(self): 
+    #     self._set_headers(200) 
+    #     response = {}  # Default response
 
-        # Parse the URL and capture the tuple that is returned
-        (resource, id) = self.parse_url(self.path) 
+    #     # Parse the URL and capture the tuple that is returned
+    #     (resource, id) = self.parse_url(self.path) 
 
-        # ANIMALS RESOURCE
-        if resource == "animals": 
-            if id is not None: 
-                response = f"{get_single_animal(id)}" 
+    #     # ANIMALS RESOURCE
+    #     if resource == "animals": 
+    #         if id is not None: 
+    #             response = f"{get_single_animal(id)}" 
 
-            else: 
-                response = f"{get_all_animals()}" 
+    #         else: 
+    #             response = f"{get_all_animals()}" 
 
-        # LOCATIONS RESOURCE
-        if resource == "locations": 
-            if id is not None: 
-                response = f"{get_single_location(id)}" 
+    #     # LOCATIONS RESOURCE
+    #     if resource == "locations": 
+    #         if id is not None: 
+    #             response = f"{get_single_location(id)}" 
 
-            else: 
-                response = f"{get_all_locations()}" 
+    #         else: 
+    #             response = f"{get_all_locations()}" 
 
-        # EMPLOYEES RESOURCE 
-        if resource == "employees": 
-            if id is not None: 
-                response = f"{get_single_employee(id)}" 
+    #     # EMPLOYEES RESOURCE 
+    #     if resource == "employees": 
+    #         if id is not None: 
+    #             response = f"{get_single_employee(id)}" 
 
-            else: 
-                response = f"{get_all_employees()}" 
+    #         else: 
+    #             response = f"{get_all_employees()}" 
 
-        # CUSTOMERS RESOURCE
-        if resource == "customers": 
-            if id is not None: 
-                response = f"{get_single_customer(id)}" 
+    #     # CUSTOMERS RESOURCE
+    #     if resource == "customers": 
+    #         if id is not None: 
+    #             response = f"{get_single_customer(id)}" 
 
-            else: 
-                response = f"{get_all_customers()}" 
+    #         else: 
+    #             response = f"{get_all_customers()}" 
 
-        self.wfile.write(response.encode()) 
+    #     self.wfile.write(response.encode()) 
 
 
     # Here's a method on the class that overrides the parent's method.
