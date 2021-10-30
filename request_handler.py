de3f17c088108af9689608e86f51b01dc00fb925
Brittany Garrett <brittanymgarrett@gmail.com>
2021-10-30 11:03:33 -0500

    Updated

--- request_handler.py
+++ request_handler.py
@@ -17,79 +17,79 @@ class HandleRequests(BaseHTTPRequestHandler):
         # path is "/animals/1", the resulting list will
         # have "" at index 0, "animals" at index 1, and "1"
         # at index 2.
-        path_params = path.split("/")
-        resource = path_params[1]
-        id = None
+        path_params = path.split("/") 
+        resource = path_params[1] 
+        id = None 
 
         # Try to get the item at index 2
-        try:
+        try: 
             # Convert the string "1" to the integer 1
             # This is the new parseInt()
-            id = int(path_params[2])
-        except IndexError:
+            id = int(path_params[2]) 
+        except IndexError: 
             pass  # No route parameter exists: /animals
-        except ValueError:
+        except ValueError: 
             pass  # Request had trailing slash: /animals/
 
-        return (resource, id)  # This is a tuple
+        return (resource, id)  # This is a tuple 
 
     # Here's a class function
-    def _set_headers(self, status):
-        self.send_response(status)
-        self.send_header('Content-type', 'application/json')
-        self.send_header('Access-Control-Allow-Origin', '*')
-        self.end_headers()
+    def _set_headers(self, status): 
+        self.send_response(status) 
+        self.send_header('Content-type', 'application/json') 
+        self.send_header('Access-Control-Allow-Origin', '*') 
+        self.end_headers() 
 
     # Another method! This supports requests with the OPTIONS verb.
-    def do_OPTIONS(self):
-        self.send_response(200)
-        self.send_header('Access-Control-Allow-Origin', '*')
-        self.send_header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
-        self.send_header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept')
-        self.end_headers()
+    def do_OPTIONS(self): 
+        self.send_response(200) 
+        self.send_header('Access-Control-Allow-Origin', '*') 
+        self.send_header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE') 
+        self.send_header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept') 
+        self.end_headers() 
 
     # Here's a method on the class that overrides the parent's method.
     # It handles any GET request.
-    def do_GET(self):
-        self._set_headers(200)
+    def do_GET(self): 
+        self._set_headers(200) 
         response = {}  # Default response
 
         # Parse the URL and capture the tuple that is returned
-        (resource, id) = self.parse_url(self.path)
+        (resource, id) = self.parse_url(self.path) 
 
         # ANIMALS RESOURCE
-        if resource == "animals":
-            if id is not None:
-                response = f"{get_single_animal(id)}"
+        if resource == "animals": 
+            if id is not None: 
+                response = f"{get_single_animal(id)}" 
 
-            else:
-                response = f"{get_all_animals()}"
+            else: 
+                response = f"{get_all_animals()}" 
 
         # LOCATIONS RESOURCE
-        if resource == "locations":
-            if id is not None:
-                response = f"{get_single_location(id)}"
+        if resource == "locations": 
+            if id is not None: 
+                response = f"{get_single_location(id)}" 
 
-            else:
-                response = f"{get_all_locations()}"
+            else: 
+                response = f"{get_all_locations()}" 
 
-        # EMPLOYEES RESOURCE
-        if resource == "employees":
-            if id is not None:
-                response = f"{get_single_employee(id)}"
+        # EMPLOYEES RESOURCE 
+        if resource == "employees": 
+            if id is not None: 
+                response = f"{get_single_employee(id)}" 
 
-            else:
-                response = f"{get_all_employees()}"
+            else: 
+                response = f"{get_all_employees()}" 
 
         # CUSTOMERS RESOURCE
-        if resource == "customers":
-            if id is not None:
-                response = f"{get_single_customer(id)}"
+        if resource == "customers": 
+            if id is not None: 
+                response = f"{get_single_customer(id)}" 
 
-            else:
-                response = f"{get_all_customers()}"
+            else: 
+                response = f"{get_all_customers()}" 
 
-        self.wfile.write(response.encode())
+        self.wfile.write(response.encode()) 
 
 
     # Here's a method on the class that overrides the parent's method.
