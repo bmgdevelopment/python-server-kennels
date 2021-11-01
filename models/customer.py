73d17e7923066ce1ed4b5f490b0a398ea3f53fae
Brittany Garrett <brittanymgarrett@gmail.com>
2021-11-01 12:22:08 -0500

    Updated

--- models/customer.py
+++ models/customer.py
@@ -1,8 +1,17 @@
+# class Customer():
+
+#     def __init__(self, id, name, address, animalId, email):
+#         self.id = id
+#         self.name = name
+#         self.address = address
+#         self.animalId = animalId
+#         self.email = email
+
 class Customer():
 
-    def __init__(self, id, name, address, animalId, email):
+    def __init__(self, id, name, address, email = "", password = ""):
         self.id = id
         self.name = name
         self.address = address
-        self.animalId = animalId
         self.email = email
+        self.password = password
